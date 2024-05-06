d5b7600645906fcc8283051c8455b411de2d542d
Cavid Abbasaliyev <cavid.abbasaliyev@kapitalbank.az>
2024-05-06 09:44:23 +0400

    Initial Commit

---
+++ DraftProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DraftProject
+//
+//  Created by cavID on 06.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
